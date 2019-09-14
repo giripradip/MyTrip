9cef7b46657cba209ff7f81a7e7d004d65a6b8ed
Pradip Giri <mepradipjee@gmail.com>
2019-09-14 09:34:19 +0200

    Add service class to sync trip info with server

---
+++ app/src/main/java/com/example/mytrip/apiinterface/TripInfoSyncService.java
@@ -0,0 +1,28 @@
+package com.example.mytrip.apiinterface;
+
+import com.example.mytrip.model.MyTripInfo;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+public interface TripInfoSyncService {
+
+    @POST("trip")
+    Call<List<MyTripInfo>> syncAllTripInfo(@Body List<MyTripInfo> myTripInfoList);
+
+    @POST("trip")
+    Call<MyTripInfo> syncCreateTripInfo(@Body MyTripInfo myTripInfo);
+
+    @PUT("trip/{id}")
+    Call<MyTripInfo> syncUpdateTripInfo(@Body MyTripInfo myTripInfo, @Path("id") int tripId);
+
+
+    @DELETE("trip/{id}")
+    Call<MyTripInfo> syncDeleteTripInfo(@Path("id") int tripId);
+}
